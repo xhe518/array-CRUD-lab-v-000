b32fbda99b5f19f0822d23c9ff816cc4718916fa
Sid He <xhe518@gmail.com>
2018-06-18 23:33:00 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  alphabet = ["b", "c", "d", "e"]
+  array = ["b", "c", "d", "e"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  array << "element"
 end
 
 def add_element_to_start_of_array(array, element)
